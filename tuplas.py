40719812034a095e0bd0a19e0745190a52d88e05
ezerosario315 <ezequiele636@gmail.com>
2023-09-23 21:29:11 +0000

    agregando las tuplas

---
+++ tuplas.py
@@ -0,0 +1,6 @@
+# es similar a las listas la diferencias es que para abrir una es con paretesis y en la lista es con corchetes
+my_tuplas=(1,2,3,4,5,6,7,8)
+print(my_tuplas)
+print(my_tuplas[1])
+print(my_tuplas.count(3))
+print(my_tuplas.index(8))
