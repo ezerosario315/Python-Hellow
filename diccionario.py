6a293368299a049c2d3ca6972113b5318e0d4095
ezerosario315 <ezequiele636@gmail.com>
2023-09-23 22:00:50 +0000

    sets y diccionario

---
+++ diccionario.py
@@ -0,0 +1,9 @@
+diccionario= {'Nombre': 'Ezequiel',
+              'Apellido':'Rosario',
+              'Cancion':"Amor prohibido",
+              'Artista':"Romeo Santos"
+              }
+print(diccionario['Apellido'])
+print(diccionario.keys())
+print(diccionario.values())
+
---
+++ sets.py
@@ -0,0 +1,5 @@
+# los sets no tiene un orden definido siempre los imprime de diferente orden y ademas no repite contenido
+My_sets ={'Python','c++',5,9}
+print(My_sets)
+My_sets.add('PHP')
+print(My_sets)
