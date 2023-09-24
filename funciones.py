9e1ac4cff769b59877acfd74efa6cf38df545452
ezerosario315 <ezequiele636@gmail.com>
2023-09-24 18:30:14 +0000

    agregando nuevos priyectos

---
+++ funciones.py
@@ -0,0 +1,6 @@
+# funones para crear una funcion ulizamos la palabra de def
+def Sumar(numero1, numero2):
+    print(numero1-numero2)
+
+Sumar(8,10)
+Sumar(20,10)
--- tuplas.py
+++ tuplas.py
@@ -1,6 +1,7 @@
-# es similar a las listas la diferencias es que para abrir una es con paretesis y en la lista es con corchetes
+# es similar a las listas la diferencias es que para abrir una es con paretesis y en la lista es con corchetes 
+#la diferencia es que no puede ser modificado
 my_tuplas=(1,2,3,4,5,6,7,8)
 print(my_tuplas)
 print(my_tuplas[1])
-print(my_tuplas.count(3))
-print(my_tuplas.index(8))
+print(my_tuplas.count(3)) # para contar cuanto 3 hay en la tupla
+print(my_tuplas.index(8))# esta cuenta en que posici[on se encuentra dicho numero]
